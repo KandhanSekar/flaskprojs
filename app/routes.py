ff6b41ce5b33dce3e812cd59098b44dc981b2610
kandhan sekar <kandhan.sekar@ibm.com>
2018-08-09 08:53:52 -0400

    solved weird errors

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from app.forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
+    form = LoginForm()
     user = {'username': 'Miguel'}
     posts = [
         {
@@ -16,9 +17,9 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts,form=form)
 
 @app.route('/login')
 def login():
-    form = LoginForm()
+
     return render_template('login.html', title='Sign In', form=form)
