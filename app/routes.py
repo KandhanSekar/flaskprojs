6311849a4a4388c41cb25c0e198d1b246c9088f2
kandhan sekar <kandhan.sekar@ibm.com>
2018-08-12 01:10:13 -0400

    improvements

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template,redirect,flash
 from app.forms import LoginForm
 
 @app.route('/')
@@ -18,8 +18,11 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts,form=form)
-
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
